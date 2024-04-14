15f1b795e82be2711cd49d45b9d6f44bd6add491
Mihnea Popescu <everything.nowro@gmail.com>
2024-04-14 18:16:51 +0300

    relationships + laravel sanctum

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -18,4 +20,14 @@ class Order extends Model
         self::ORDER_STATUS_CANCELED,
         self::ORDER_STATUS_FINISHED
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
